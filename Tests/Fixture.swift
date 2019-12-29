f5b55c50c7772d5d5408919ae0888d4cf7c3c84a
Erk Ekin <erkekin@gmail.com>
2019-12-29 11:29:54 +0000

    init

---
+++ Tests/Fixture.swift
@@ -0,0 +1,28 @@
+// ExampleClass
+public final class ExampleClass: NSObject {
+  let exampleProperty: Int = 1234
+}
+
+// ExampleStruct
+internal struct ExampleStruct: Equatable {
+  let exampleProperty: Int = 1234
+}
+
+
+// ExampleEnum
+enum ExampleEnum {
+  static let  a =
+  399
+  case b(firstOne:String,
+    secondOne: Int)
+  static let  fdfdf =
+  399
+  case v
+  case we
+  case aasd
+}
+
+
+
+// ExampleProtocol
+protocol ExampleProtocol {}
